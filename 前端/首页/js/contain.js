f472fa5df37627048f4d261e3dd383128fe697ff
cynczl <605947802@qq.com>
2016-10-20 23:03:18 +0800

    contain的一些修复
    左边的颜色和间距

--- 前端/首页/js/contain.js
+++ 前端/首页/js/contain.js
@@ -7,14 +7,14 @@ window.onload = function(){
 		nCl[i].onclick = function(){
 			for(var n= 0;n<len;n++){
 				var nCu1 = nCl[n].getElementsByTagName("ul");
-	            if(nCu1.length!=0){
-	            	nCu1[0].className = "hide";
-	            }
+	            // if(nCu1.length!=0){
+	            // 	nCu1[0].className = "hide";
+	            // }
 	            nCl[n].className = "";
 	        }
 			this.className = "on";
-			var nCu2 = nCl[this.index].getElementsByTagName("ul");
-			nCu2[0].className = "navSlide";
+			// var nCu2 = nCl[this.index].getElementsByTagName("ul");
+			// nCu2[0].className = "navSlide";
 		}
 	}
 }
