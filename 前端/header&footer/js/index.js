f45d43c6d6325daea4f43c88ce842e2a1ae5c654
Iam5B <1062831461@qq.com>
2016-10-21 20:02:07 +0800

    删了一个没用的文件夹

--- 前端/header&footer/js/index.js
+++
@@ -1,6 +0,0 @@
-$(".navSlideDown").css("display","none");
-		$(".navBlock").hover(function(){
-    		$(this).children(".navSlideDown").stop(true, false).slideDown();
-		}, function(){
-    	$(this).children(".navSlideDown").stop(true, false).slideUp();
-		});
