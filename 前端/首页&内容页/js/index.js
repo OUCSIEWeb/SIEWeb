a5e9b2afd32f36fd9ba3c5597bcb01d1b7412535
friendllcc <759894921@qq.com>
2016-10-24 16:01:09 +0800

    improve
    css and js have been improved while the html has no change

--- 前端/首页&内容页/js/index.js
+++ 前端/首页&内容页/js/index.js
@@ -1,6 +1,19 @@
-$(".navSlideDown").css("display","none");
+window.onload = function(){
+		$(".navSlideDown").css("display","none");
 		$(".navBlock").hover(function(){
     		$(this).children(".navSlideDown").stop(true, false).slideDown();
 		}, function(){
     	$(this).children(".navSlideDown").stop(true, false).slideUp();
 		});
+
+		$(".indexMainTittleLeft").click(function(){
+			$(".indexMainTittleLeft").removeClass("mainThis");
+			$(this).addClass("mainThis");
+		})
+
+		// $(".search>input").focus(function(){
+		// 	$(this).placeholder('');
+		// },$(".search>input").blur(function(){
+		// 	$(this).placeholder('搜索');
+		// }))
+	}
