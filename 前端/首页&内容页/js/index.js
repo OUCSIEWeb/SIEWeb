3235cb6075e3e3a41f7a70e36ec30cac8ce1f0dc
friendllcc <759894921@qq.com>
2016-10-29 13:10:49 +0800

    改闪动效果

--- 前端/首页&内容页/js/index.js
+++ 前端/首页&内容页/js/index.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-		$(".navSlideDown").css("display","none");
+		// $(".navSlideDown").css("display","none");
 		$("ul.notice").css("display","none");
 		$(".navBlock").hover(function(){
     		$(this).children(".navSlideDown").stop(true, false).slideDown();
