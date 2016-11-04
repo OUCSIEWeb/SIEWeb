a41a576f5f0559fa180e8af2ffd8449580365833
friendllcc <759894921@qq.com>
2016-11-05 01:03:10 +0800

    前端已经搞定，学长你加油

--- 前端/首页&内容页/js/index.js
+++ 前端/首页&内容页/js/index.js
@@ -4,20 +4,31 @@ window.onload = function(){
 		$(".navBlock").hover(function(){
     		$(this).children(".navSlideDown").stop(true, false).slideDown();
 		}, function(){
-    	$(this).children(".navSlideDown").stop(true, false).slideUp();
+    	$(this).children(".navSlideDown").stop(true, false).css("display","none");
 		});
 
-		$(".indexMainTittleLeft").click(function(){
-			$(".indexMainTittleLeft").removeClass("mainThis");
-			$(this).addClass("mainThis");
+		$(".navSlideDown").hover(function(){
+			$($(this).parent().children()[0]).css("color","white");
+		},function(){
+			$($(this).parent().children()[0]).css("color","#404040");
+		})
+		$(".navBlock>a").hover(function(){
+			$(this).css("color","white");
+		},function(){
+			$(this).css("color","#404040");
 		})
 
-		$(".indexMainTittleSecond").click(function(){
-			$("ul.notice").css("display","block");
-			$("ul.news").css("display","none");
-		})
-		$(".indexMainTittleFirst").click(function(){
-			$("ul.notice").css("display","none");
-			$("ul.news").css("display","block");
-		})
+		// $(".indexMainTittleLeft").click(function(){
+		// 	$(".indexMainTittleLeft").removeClass("mainThis");
+		// 	$(this).addClass("mainThis");
+		// })
+
+		// $(".indexMainTittleSecond").click(function(){
+		// 	$("ul.notice").css("display","block");
+		// 	$("ul.news").css("display","none");
+		// })
+		// $(".indexMainTittleFirst").click(function(){
+		// 	$("ul.notice").css("display","none");
+		// 	$("ul.news").css("display","block");
+		// })
 	}
